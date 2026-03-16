52c64e6698b982945782aef46cd9df8befff25e5
baskaure <aurelien.branco@ynov.com>
2026-03-16 17:01:21 +0100

    kingdomads

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,26 @@
 import { openCalendly } from '../constants'
 
 export default function Nav() {
+  const isKingdomAds =
+    typeof window !== 'undefined' &&
+    window.location &&
+    window.location.pathname.startsWith('/kingdomads')
+
   return (
     <nav>
       <div className="nav-container">
         <div className="logo">
           <img src="/img/Or_blanc-petit.png" alt="Kingdom Ads" />
         </div>
-        <ul className="nav-links">
-          <li><a href="#how-it-works">Fonctionnement</a></li>
-          <li><a href="#process">Processus</a></li>
-          <li><a href="#testimonials">Témoignages</a></li>
-          <li><a href="#team">Équipe</a></li>
-          <li><a href="#cta">Appel</a></li>
-        </ul>
+        {!isKingdomAds && (
+          <ul className="nav-links">
+            <li><a href="#how-it-works">Fonctionnement</a></li>
+            <li><a href="#process">Processus</a></li>
+            <li><a href="#testimonials">Témoignages</a></li>
+            <li><a href="#team">Équipe</a></li>
+            <li><a href="#cta">Appel</a></li>
+          </ul>
+        )}
         <button type="button" onClick={openCalendly} className="cta-nav">Démarrer</button>
       </div>
     </nav>
